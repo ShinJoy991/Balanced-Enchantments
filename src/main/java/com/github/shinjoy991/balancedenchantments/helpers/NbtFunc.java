5b6e01787891043e1757a1682ac02d1c2f08f79a
ShinJoy991 <shinjoytestmail@gmail.com>
2024-05-24 01:24:14 +0700

    license+descriptions

--- src/main/java/com/github/shinjoy991/balancedenchantments/helpers/NbtFunc.java
+++ src/main/java/com/github/shinjoy991/balancedenchantments/helpers/NbtFunc.java
@@ -1,12 +1,6 @@
 package com.github.shinjoy991.balancedenchantments.helpers;
 
-import com.google.common.collect.ArrayListMultimap;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-
-import java.util.List;
 
 public class NbtFunc {
     public static CompoundNBT copyNBTWithoutUUID(CompoundNBT oldNbt) {
